ec26907a60fb7e8babd815a4d50e37df7f0951eb
Claus Rørbech <claus.rorbech@gmail.com>
2020-09-03 10:10:42 +0200

    Upgrade to Kotlin 1.4.0 (#1)

--- compiler-plugin/src/main/kotlin/dev/nhachicha/AccessorModifierPlugin.kt
+++ compiler-plugin/src/main/kotlin/dev/nhachicha/AccessorModifierPlugin.kt
@@ -17,36 +17,26 @@
 package dev.nhachicha
 
 import com.google.auto.service.AutoService
-import com.intellij.mock.MockProject
-import com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.backend.common.BackendContext
 import org.jetbrains.kotlin.backend.common.FileLoweringPass
+import org.jetbrains.kotlin.backend.common.IrElementTransformerVoidWithContext
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
-import org.jetbrains.kotlin.backend.common.lower.createIrBuilder
-import org.jetbrains.kotlin.backend.common.lower.irBlock
+import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
+import org.jetbrains.kotlin.com.intellij.mock.MockProject
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.ir.IrElement
 import org.jetbrains.kotlin.ir.IrStatement
-import org.jetbrains.kotlin.ir.builders.createTmpVariable
-import org.jetbrains.kotlin.ir.builders.irCall
-import org.jetbrains.kotlin.ir.builders.irGet
-import org.jetbrains.kotlin.ir.builders.irString
+import org.jetbrains.kotlin.ir.builders.*
 import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.ir.declarations.IrFunction
+import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
 import org.jetbrains.kotlin.ir.declarations.isPropertyAccessor
 import org.jetbrains.kotlin.ir.expressions.IrExpression
-import org.jetbrains.kotlin.ir.expressions.IrGetField
-import org.jetbrains.kotlin.ir.types.isNullableAny
+import org.jetbrains.kotlin.ir.expressions.IrReturn
 import org.jetbrains.kotlin.ir.types.isNullableString
 import org.jetbrains.kotlin.ir.types.isString
-import org.jetbrains.kotlin.ir.util.constructors
-import org.jetbrains.kotlin.ir.util.functions
 import org.jetbrains.kotlin.ir.util.isGetter
-import org.jetbrains.kotlin.ir.util.statements
 import org.jetbrains.kotlin.ir.visitors.*
-import org.jetbrains.kotlin.name.Name
-import org.jetbrains.kotlin.resolve.BindingContext
 
 @AutoService(ComponentRegistrar::class)
 class AccessorModifierComponentRegistrar : ComponentRegistrar {
@@ -55,7 +45,7 @@ class AccessorModifierComponentRegistrar : ComponentRegistrar {
     }
 
     companion object {
-        fun registerExtensions(project: Project, configuration: CompilerConfiguration) {
+        fun registerExtensions(project: MockProject, configuration: CompilerConfiguration) {
             IrGenerationExtension.registerExtension(
                     project,
                     AccessorModifierIrGenerationExtension()
@@ -66,76 +56,38 @@ class AccessorModifierComponentRegistrar : ComponentRegistrar {
 
 class AccessorModifierIrGenerationExtension : IrGenerationExtension {
 
-    override fun generate(
-            f: IrFile,
-            backendContext: BackendContext,
-            bindingContext: BindingContext
-    ) {
-        AccessorCallTransformer(backendContext).runOnFileInOrder(f)
+    override fun generate(moduleFragment: IrModuleFragment, pluginContext: IrPluginContext) {
+        for (file in moduleFragment.files) {
+            AccessorCallTransformer(pluginContext).runOnFileInOrder(file)
+        }
     }
 
     class AccessorCallTransformer(
-            val context: BackendContext
-    ) : IrElementTransformerVoid(), FileLoweringPass {
-
-        private val nameToString = Name.identifier("toString")
-        private val nameAppend = Name.identifier("append")
-        private val stringBuilder = context.ir.symbols.stringBuilder.owner
-        private val prefix = "Hello "
-
-        private val constructor = stringBuilder.constructors.single {
-            it.valueParameters.size == 0
-        }
-
-        private val toStringFunction = stringBuilder.functions.single {
-            it.valueParameters.size == 0 && it.name == nameToString
-        }
-
-        private val appendFunction = stringBuilder.functions.single {
-            it.name == nameAppend &&
-                    it.valueParameters.size == 1 &&
-                    it.valueParameters.single().type.isNullableAny()
-        }
+            val context: IrPluginContext
+    ) : IrElementTransformerVoidWithContext(), FileLoweringPass {
 
         override fun lower(irFile: IrFile) {
             irFile.transformChildrenVoid()
         }
 
-        override fun visitFunction(declaration: IrFunction): IrStatement {
-
-            // Modify only property accessor getter for type String or nullable String
+        override fun visitFunctionNew(declaration: IrFunction): IrStatement {
             return if (declaration.isPropertyAccessor
                     && declaration.isGetter
                     && (declaration.returnType.isString() || declaration.returnType.isNullableString())
             ) {
-                declaration.transformChildrenVoid(this)
-                for (statement in declaration.body?.statements!!) {
-                    statement.transformChildrenVoid(object : IrElementTransformerVoid() {
-                        override fun visitGetField(expression: IrGetField): IrExpression {
-                            return context.createIrBuilder(expression.symbol, expression.startOffset, expression.endOffset).irBlock(expression) {
-                                val stringBuilderImpl = createTmpVariable(irCall(constructor))
-
-                                +irCall(appendFunction).apply {
-                                    dispatchReceiver = irGet(stringBuilderImpl)
-                                    putValueArgument(0, irString(prefix)) // appending PROPERTY_BACKING_FIELD
-                                }
-
-                                +irCall(appendFunction).apply {
-                                    dispatchReceiver = irGet(stringBuilderImpl)
-                                    putValueArgument(0, expression) // appending PROPERTY_BACKING_FIELD
-                                }
-                                // create an irCall and add it to this IrBuilder block
-                                +irCall(toStringFunction).apply {
-                                    dispatchReceiver /* <-- to String called on --> */ = irGet(stringBuilderImpl)
-                                }
-                            }
+                declaration.body?.transformChildrenVoid(object : IrElementTransformerVoid() {
+                    override fun visitReturn(expression: IrReturn): IrExpression {
+                        return IrBlockBuilder(context, currentScope?.scope!!, expression.startOffset, expression.endOffset).irBlock {
+                            val irConcat = irConcat()
+                            irConcat.addArgument(irString("Hello "))
+                            irConcat.addArgument(expression.value)
+                            +irReturn(irConcat)
                         }
-                    })
-                }
-
-                super.visitFunction(declaration)
+                    }
+                })
+                super.visitFunctionNew(declaration)
             } else {
-                super.visitFunction(declaration)
+                super.visitFunctionNew(declaration)
             }
         }
 
@@ -154,4 +106,3 @@ fun FileLoweringPass.runOnFileInOrder(irFile: IrFile) {
         }
     })
 }
-
